e0fdccf811c11f49590ee1307de6bcecc55c61d2
Tim Quattrochi <timq82@gmail.com>
2023-10-12 00:24:06 -0400

    added some more inputs, labels, min styling, delete console logs

--- src/components/JobForm/JobForm.jsx
+++ src/components/JobForm/JobForm.jsx
@@ -12,17 +12,18 @@ const categories = [
 const initialJobState = {
   title: "",
   company: "",
+  dateApplied: Date.now(),
+  additional: "",
   status: "",
 };
 
 // eslint-disable-next-line react/prop-types
 function JobForm({ addJob }) {
   const [jobData, setJobData] = useState(initialJobState);
-  const [selectedCategory, setSelectedCategory] = useState("");
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    console.log(name);
+
     if (name === "status") {
       setJobData({
         ...jobData,
@@ -43,24 +44,42 @@ function JobForm({ addJob }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col p-2 m-2 gap-1"
+    >
+      <label htmlFor="title">Job Title: </label>
       <input
         type="text"
         name="title"
+        id="title"
         placeholder="Job Title"
+        className="p-2"
         value={jobData.title}
         onChange={handleInputChange}
       />
+
+      <label htmlFor="company" className="py-2">
+        Company:
+      </label>
       <input
         type="text"
         name="company"
+        id="company"
+        className="p-2"
         placeholder="Company"
         value={jobData.company}
         onChange={handleInputChange}
       />
+
+      <label htmlFor="category" className="py-2">
+        Status:
+      </label>
       <select
         role="combobox"
         name="status"
+        id="category"
+        className="p-2"
         value={jobData.status}
         onChange={handleInputChange}
       >
@@ -70,7 +89,20 @@ function JobForm({ addJob }) {
           </option>
         ))}
       </select>
-      <button type="submit">Add Job</button>
+
+      <label htmlFor="additional" className="flex flex-col py-2">
+        Additional Notes:
+      </label>
+      <textarea id="additional" className="mt-1">
+        {jobData.additional}
+      </textarea>
+
+      <button
+        type="submit"
+        className="mt-5 border w-20 mx-auto rounded"
+      >
+        Add Job
+      </button>
     </form>
   );
 }
