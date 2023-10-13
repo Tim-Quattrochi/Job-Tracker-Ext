90a42215e52d570d306ceb67527d02c855f605e2
Tim Quattrochi <timq82@gmail.com>
2023-10-13 12:22:40 -0400

    log in user controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -37,4 +37,47 @@ const registerUser = async (req, res) => {
   });
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    return res
+      .status(400)
+      .json({ message: "please fill out all required fields" });
+
+  try {
+    User.login(email, password, (err, user) => {
+      if (err?.message === "No user found.") {
+        return res
+          .status(401)
+          .json({ message: "Check credentials." });
+      } else if (err) {
+        return res.status(500).json({
+          message:
+            err.message || "An error occurred while signing in.",
+        });
+      } else {
+        //create the access token and payload to send in response.
+        const payload = { id: user.id, email: user.email };
+        const accessToken = User.createAccessToken(payload);
+        const refreshToken = User.createRefreshToken(payload);
+
+        //save the refresh token to the db.
+        User.saveRefreshToken(user.id, refreshToken);
+
+        //send the http only cookie.
+        User.createCookie(refreshToken, res);
+
+        //delete sensitive data from response to client.
+
+        delete user.refresh_token;
+
+        res.status(200).json({ user: { ...user, accessToken } });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { registerUser, loginUser };
