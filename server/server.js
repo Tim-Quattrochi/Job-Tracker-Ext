59ee79668879dc1454ea6e16490ac1237e8906b6
Tim Quattrochi <timq82@gmail.com>
2023-10-12 23:32:52 -0400

    add userRouter to server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const jobDetailsRouter = require("./routes/jobDetails.routes");
+const userRouter = require("./routes/user.routes");
 const { PORT } = require("./config/constants");
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/details", jobDetailsRouter);
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}.`);
