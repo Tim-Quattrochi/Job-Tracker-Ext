f6ca5d27bfcd587d931db3f92931ae43b8a7e022
Tim Quattrochi <timq82@gmail.com>
2023-10-13 11:16:00 -0400

    updated constants

--- server/config/constants.js
+++ server/config/constants.js
@@ -3,5 +3,19 @@ const HOST = process.env.HOST;
 const USER_NAME = process.env.USER_NAME;
 const PASSWORD = process.env.PASSWORD;
 const DB_NAME = process.env.DB_NAME;
+const ACCESS_EXPIRES_IN = process.env.ACCESS_EXPIRES_IN;
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+const REFRESH_EXPIRES_IN = process.env.REFRESH_EXPIRES_IN;
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
 
-module.exports = { PORT, HOST, USER_NAME, PASSWORD, DB_NAME };
+module.exports = {
+  PORT,
+  HOST,
+  USER_NAME,
+  PASSWORD,
+  DB_NAME,
+  ACCESS_EXPIRES_IN,
+  ACCESS_TOKEN_SECRET,
+  REFRESH_EXPIRES_IN,
+  REFRESH_TOKEN_SECRET,
+};
