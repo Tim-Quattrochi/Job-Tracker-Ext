b27538e6d93643e0b6eaef74764fe7ca25fa3aa6
Tim Quattrochi <timq82@gmail.com>
2023-10-13 12:40:12 -0400

    add try catch to registerUser

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -7,34 +7,38 @@ const registerUser = async (req, res) => {
       .status(400)
       .json({ message: "please fill out all required fields" });
   }
+  try {
+    const newPasswordHash = await User.hashPassword(confirmPassword);
 
-  const newPasswordHash = await User.hashPassword(confirmPassword);
+    const userDetails = new User({
+      name,
+      email,
+      password: newPasswordHash,
+    });
 
-  const userDetails = new User({
-    name,
-    email,
-    password: newPasswordHash,
-  });
+    //saving to db.
+    User.create(userDetails, (err, user) => {
+      if (err) {
+        res.status(500).json({
+          message:
+            err.message ||
+            "An error occurred while creating the job details",
+        });
+      } else {
+        //send back the user minus the password.
+        delete user.password;
 
-  //saving to db.
-  User.create(userDetails, (err, user) => {
-    if (err) {
-      res.status(500).json({
-        message:
-          err.message ||
-          "An error occurred while creating the job details",
-      });
-    } else {
-      //send back the user minus the password.
-      delete user.password;
+        //create the access token and payload to send in response.
+        const payload = { id: user.id, email: user.email };
+        const accessToken = User.createAccessToken(payload);
 
-      //create the access token and payload to send in response.
-      const payload = { id: user.id, email: user.email };
-      const accessToken = User.createAccessToken(payload);
-
-      res.status(201).json({ user: { ...user, accessToken } });
-    }
-  });
+        res.status(201).json({ user: { ...user, accessToken } });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const loginUser = async (req, res) => {
