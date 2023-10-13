265a2b5ffafc2f66926d301097923d5a92b8951b
Tim Quattrochi <timq82@gmail.com>
2023-10-13 12:22:00 -0400

    login method, save tokens and create cookie on User

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,6 +4,9 @@ const jwt = require("jsonwebtoken");
 const {
   ACCESS_TOKEN_SECRET,
   ACCESS_EXPIRES_IN,
+  REFRESH_EXPIRES_IN,
+  REFRESH_TOKEN_SECRET,
+  APP_NAME,
 } = require("../config/constants");
 
 class User {
@@ -28,6 +31,32 @@ class User {
     });
   }
 
+  static login(email, password, result) {
+    sql.query(
+      `SELECT * FROM users WHERE email = ?`,
+      email,
+      async (err, res) => {
+        if (err) {
+          return result(err, null);
+        }
+        if (res.length === 0) {
+          return result({ message: "No user found." }, null);
+        } else if (res.length) {
+          //if there is a user found, we will check the password against the db.
+          const doesPasswordMatch = await bcrypt.compare(
+            password,
+            res[0].password
+          );
+          if (doesPasswordMatch) {
+            //password matches, send user minus the password hash.
+            delete res[0].password;
+            return result(null, res[0]);
+          }
+        }
+      }
+    );
+  }
+
   static async hashPassword(password) {
     try {
       const passwordHash = await bcrypt.hash(password, 10);
@@ -42,6 +71,35 @@ class User {
       expiresIn: ACCESS_EXPIRES_IN,
     });
     return accessToken;
+  }
+
+  static createRefreshToken(payload) {
+    const refreshToken = jwt.sign(payload, REFRESH_TOKEN_SECRET, {
+      expiresIn: REFRESH_EXPIRES_IN,
+    });
+    return refreshToken;
+  }
+
+  static saveRefreshToken(userId, refreshToken) {
+    sql.query(
+      "UPDATE users SET refresh_token = ? WHERE id = ?",
+      [refreshToken, userId],
+      (err, res) => {
+        if (err) {
+          console.log("error: ", err);
+        }
+      }
+    );
+    return refreshToken;
+  }
+
+  static createCookie(token, response) {
+    return response.cookie(APP_NAME, token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: 7 * 24 * 60 * 60 * 1000, //7 days
+    });
   }
 }
 
