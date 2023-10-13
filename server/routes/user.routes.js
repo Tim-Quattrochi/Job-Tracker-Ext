785b9f963ab212d4e710041f9f1d1c63cd7ff80c
Tim Quattrochi <timq82@gmail.com>
2023-10-13 12:21:06 -0400

    add login route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,5 +2,6 @@ const userRouter = require("express").Router();
 const user = require("../controllers/user.controller");
 
 userRouter.post("/register", user.registerUser);
+userRouter.post("/login", user.loginUser);
 
 module.exports = userRouter;
