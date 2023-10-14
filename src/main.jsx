a1c0b88cf67f1fb6e156d59567399f8a389e2dea
Tim Quattrochi <timq82@gmail.com>
2023-10-14 15:11:24 -0400

    deleted the localStorage check from main

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import store from "./app/store.jsx";
+import { Provider } from "react-redux";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
