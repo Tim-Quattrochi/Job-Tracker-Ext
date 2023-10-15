1173e34da70bf9065f3fe1355f01598fe900a054
Tim Quattrochi <timq82@gmail.com>
2023-10-15 01:06:23 -0400

    added verifyJWT middleware to getAllJobsByUserId route

--- server/routes/jobDetails.routes.js
+++ server/routes/jobDetails.routes.js
@@ -1,12 +1,17 @@
 const jobDetailsRouter = require("express").Router();
 const jobDetails = require("../controllers/jobDetails.controller.js");
+const verifyJWT = require("../middleware/verifyJWT.js");
 
 jobDetailsRouter.post("/", jobDetails.createJobDetails);
 
 jobDetailsRouter.get("/:id", jobDetails.getOneJob);
 
 jobDetailsRouter.get("/job/:status", jobDetails.findAllByStatus);
-jobDetailsRouter.get("/jobs/:id", jobDetails.getAllJobsByUserId);
+jobDetailsRouter.get(
+  "/jobs/:id",
+  verifyJWT,
+  jobDetails.getAllJobsByUserId
+);
 
 jobDetailsRouter.put("/:id", jobDetails.updateJob);
 
