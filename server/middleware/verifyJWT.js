afc841b986b629fb4da88de04d022a680f422641
Tim Quattrochi <timq82@gmail.com>
2023-10-15 01:02:24 -0400

    got rid of if statement as it was not needed

--- server/middleware/verifyJWT.js
+++ server/middleware/verifyJWT.js
@@ -3,34 +3,30 @@ const jwt = require("jsonwebtoken");
 const { ACCESS_TOKEN_SECRET } = require("../config/constants");
 
 const verifyJWT = async (req, res, next) => {
-  if (req.headers.authorization) {
-    const header =
-      req.headers.authorization || req.headers.Authorization;
+  const header =
+    req.headers.authorization || req.headers.Authorization;
 
-    if (!header?.startsWith("Bearer")) {
-      return res.status(401).json({ message: "Unauthorized" });
-    }
+  if (!header?.startsWith("Bearer")) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
-    const accessToken = header.split(" ")[1];
+  const accessToken = header.split(" ")[1];
 
-    try {
-      jwt.verify(accessToken, ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-          console.log(err);
-          return res.status(403).json({ message: "Forbidden." });
-        } else {
-          req.user = {
-            id: decoded.id,
-            email: decoded.email,
-          };
+  try {
+    jwt.verify(accessToken, ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        console.log(err);
+        return res.status(403).json({ message: "Forbidden." });
+      } else {
+        req.userId = decoded.id;
+        req.email = decoded.email;
 
-          next();
-        }
-      });
-    } catch (error) {
-      console.log(error);
-      return res.status(403).json({ message: "Forbidden." });
-    }
+        next();
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(403).json({ message: "Forbidden." });
   }
 };
 
