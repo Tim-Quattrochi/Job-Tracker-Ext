17e5ae5663e564c897c585ad819990af5de9d9ce
Tim Quattrochi <timq82@gmail.com>
2023-10-15 01:03:31 -0400

    add morgan logger, add origin to server.js

--- server/server.js
+++ server/server.js
@@ -2,15 +2,22 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
 const jobDetailsRouter = require("./routes/jobDetails.routes");
 const userRouter = require("./routes/user.routes");
 const { PORT } = require("./config/constants");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173/", "http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/details", jobDetailsRouter);
