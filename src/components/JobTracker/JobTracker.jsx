2957e432aa2282c3e487d1e48f9c0862fee460c0
Tim Quattrochi <timq82@gmail.com>
2023-10-16 00:15:57 -0400

    bring in user state, pass addJob to JobList

--- src/components/JobTracker/JobTracker.jsx
+++ src/components/JobTracker/JobTracker.jsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
 import JobForm from "../JobForm/JobForm";
 import JobList from "../JobList/JobList";
+import { useAuth } from "../../hooks/useAuth";
 
 const JobTracker = () => {
   const [jobs, setJobs] = useState([]);
-
+  const { user } = useAuth();
   const addJob = (job) => {
     setJobs([...jobs, job]);
   };
+
   return (
     <main className=" ">
       <h1 className="text-polo-blue-900 text-center text-4xl font-semibold mt-10">
-        My Job Tracker
+        <div className="my-2 font-medium">{user && user.name} 👋</div>
+        <span className="p-2 ">Welcome to your Job Tracker</span>
       </h1>
       <JobForm addJob={addJob} />
-      <JobList jobs={jobs} />
+      <JobList jobs={jobs} addJob={addJob} />
     </main>
   );
 };
