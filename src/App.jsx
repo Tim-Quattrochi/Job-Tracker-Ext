dcd72dec7eebd395f0d6858a76b065707270d287
Tim Quattrochi <timq82@gmail.com>
2023-10-18 00:09:19 -0400

    added placeholder logo for now on App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import JobTracker from "./components/JobTracker/JobTracker";
 import Login from "./pages/Login/Login";
+import logo from "./assets/logo.svg";
 
 function App() {
   return (
-    <>
+    <div className="">
+      <img
+        src={logo}
+        width="275px"
+        alt=""
+        style={{ marginTop: "10px", marginLeft: "15px" }}
+      />
       <Routes>
         <Route path="/" element={<JobTracker />} />
         <Route path="/login" element={<Login />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
