52d8bbf9ca9d36e22a6a795647759e066574b860
Tim Quattrochi <timq82@gmail.com>
2023-10-17 20:18:29 -0400

    component for rendering the rich text

---
+++ src/components/TextEditor/RenderRichText.jsx
@@ -0,0 +1,59 @@
+import { useEffect } from "react";
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import Highlight from "@tiptap/extension-highlight";
+import { Color } from "@tiptap/extension-color";
+import TextStyle from "@tiptap/extension-text-style";
+
+const extensions = [
+  StarterKit.configure({
+    bulletList: {
+      keepMarks: true,
+      keepAttributes: false,
+    },
+    orderedList: {
+      keepMarks: true,
+      keepAttributes: false,
+    },
+  }),
+  Highlight,
+
+  TextStyle,
+  Color.configure({
+    types: ["textStyle"],
+  }),
+];
+
+const RenderRichText = ({ content }) => {
+  const editor = useEditor({
+    extensions,
+    editable: false,
+    editorProps: {
+      attributes: {
+        class:
+          "prose lg:prose-xl h-52 bg-white overflow-y-auto focus:outline-none min-h-[300px]",
+      },
+    },
+  });
+
+  const parsedContent = JSON.parse(content);
+
+  useEffect(() => {
+    if (parsedContent) {
+      editor?.commands.setContent(parsedContent);
+    }
+  }, [parsedContent, editor]);
+
+  return (
+    <EditorContent
+      extensions={extensions}
+      editor={editor}
+      content={content}
+      name="additional"
+      value={content}
+      id="additionalDetails"
+    />
+  );
+};
+
+export default RenderRichText;
