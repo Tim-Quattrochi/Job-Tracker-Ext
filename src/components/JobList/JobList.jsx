19266b51a51e4bf39eaa906810d71a5991b4483b
Tim Quattrochi <timq82@gmail.com>
2023-10-20 11:42:29 -0400

    button to add new job with modal

--- src/components/JobList/JobList.jsx
+++ src/components/JobList/JobList.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "../../hooks/useAuth";
 import JobTable from "../JobForm/JobTable/JobTable";
 import ShowError from "../ShowError/ShowError";
@@ -15,6 +15,8 @@ import {
   useDeleteJobMutation,
   useGetJobByUserIdQuery,
 } from "../../features/jobs/jobApi";
+import Modal from "../Modal/Modal";
+import JobForm from "../JobForm/JobForm";
 
 const options = {
   keys: ["companyName", "status"],
@@ -22,6 +24,7 @@ const options = {
 };
 
 function JobList() {
+  const [showModal, setShowModal] = useState(false);
   const { user } = useAuth();
 
   const { data, error, isLoading } = useGetJobByUserIdQuery(user?.id);
@@ -83,13 +86,16 @@ function JobList() {
         <ShowError errorMsg="Error while deleting job." />
       )}
 
-      <div className="p-4">
+      <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+        <JobForm setShowModal={setShowModal} />
+      </Modal>
+      <div className="flex  flex-wrap justify-start p-4">
         <Search
           search={searchWord}
           setSearchWord={(value) => dispatch(setSearchWord(value))}
         />
         <select
-          className="p-2 border border-gray-300 rounded mb-4"
+          className="p-2 border border-gray-300 rounded ml-2 mb-4"
           value={selectedStatus}
           onChange={(e) =>
             dispatch(setSelectedStatus(e.target.value))
@@ -102,6 +108,45 @@ function JobList() {
           <option value="Offered">Offered</option>
           <option value="Accepted">Accepted</option>
         </select>
+        <button
+          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center ml-auto"
+          onClick={() => setShowModal(true)}
+        >
+          <svg
+            width="20"
+            height="40"
+            viewBox="0 0 50 50"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className=" w-4 h-4 mr-2"
+          >
+            <circle
+              cx="25"
+              cy="25"
+              r="23"
+              stroke="#797373"
+              strokeWidth="4"
+            />
+            <rect
+              x="12"
+              y="23"
+              width="26"
+              height="4"
+              rx="1"
+              fill="#23581b"
+            />
+            <rect
+              x="27"
+              y="12"
+              width="26"
+              height="4"
+              rx="1"
+              transform="rotate(90 27 12)"
+              fill="#23581b"
+            />
+          </svg>
+          Add new job
+        </button>
         <JobTable
           data={data}
           results={results}
