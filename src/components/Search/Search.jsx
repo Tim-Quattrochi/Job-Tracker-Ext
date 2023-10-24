3e4fe212b5310af68a90ee3c1609dc91dfb18dc5
Tim Quattrochi <timq82@gmail.com>
2023-10-24 17:35:06 -0400

    took out margin bottom

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -13,7 +13,7 @@ const Search = ({ searchWord, setSearchWord }) => {
         placeholder="Search by company"
         value={searchWord}
         onChange={(e) => setSearchWord(e.target.value.toLowerCase())}
-        className="block h-10 w-56 rounded-lg  text-opacity-30  leading-normal px-5 placeholder:font-normal  placeholder:not-italic border text-base border-primary-500 focus:ring-1 focus:outline-none  focus:ring-primary-700 focus:border mb-4 pl-10"
+        className="block h-10 w-56 rounded-lg  text-opacity-30  leading-normal px-5 placeholder:font-normal  placeholder:not-italic border text-base border-primary-500 focus:ring-1 focus:outline-none  focus:ring-primary-700 focus:border pl-10"
       />
     </label>
   );
