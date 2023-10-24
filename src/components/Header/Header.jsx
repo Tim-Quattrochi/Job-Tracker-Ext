b4a2e7d150a0a25d442347f5e5a134d2141b2047
Tim Quattrochi <timq82@gmail.com>
2023-10-24 17:53:16 -0400

    add margin to inner div

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import icon from "../../assets/Icon.svg";
 const Header = () => {
   return (
     <header className="w-full flex flex-col py-2 h-auto  border-primary-200 border-b  ">
-      <div className="flex h-14 max-w-7xl px-8 py-0 items-center  self-stretch">
+      <div className="flex h-14 max-w-7xl gap-2  px-8 py-0 items-center mx-20 self-stretch">
         <span className="h-5 mb-1.5 mr-1 self-center">
           <img src={icon} />
         </span>
