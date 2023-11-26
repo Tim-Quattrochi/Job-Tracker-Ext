18a42baf84c4dccf772b25723739d21560faa7e1
Tim Quattrochi <timq82@gmail.com>
2023-11-26 15:33:21 -0500

    Refactor jobsSlice.js to use removeJob and isInEdit actions

--- src/features/jobs/jobsSlice.js
+++ src/features/jobs/jobsSlice.js
@@ -18,18 +18,30 @@ const jobsSlice = createSlice({
     addJob: (state, action) => {
       state.data.push(action.payload);
     },
-    deleteJob: (state, action) => {
+    removeJob: (state, action) => {
       state.data = state.data.filter(
-        (job) => job.id !== action.payload
+        (job) => job.id !== action.payload.id
       );
     },
     updateJob: (state, action) => {
-      const { id, updatedJob } = action.payload;
+      const { id } = action.payload;
+
       const index = state.data.findIndex((job) => job.id === id);
       if (index !== -1) {
-        state.data[index] = { ...state.data[index], ...updatedJob };
+        state.data[index] = {
+          ...state.data[index],
+          ...action.payload,
+        };
       }
     },
+    isInEdit: (state, action) => {
+      const { id, inEdit } = action.payload;
+      const index = state.data.findIndex((job) => job.id === id);
+      if (index !== -1) {
+        state.data[index] = { ...state.data[index], inEdit };
+      }
+    },
+
     setError: (state, action) => {
       state.status = "failed";
       state.error = action.payload;
@@ -41,13 +53,19 @@ const jobsSlice = createSlice({
   },
 });
 
+export const selectJobInEdit = (state) => {
+  if (state.jobs.data === undefined) return null;
+  return state.jobs.data.find((job) => job.inEdit === true);
+};
+
 export const {
   setJobs,
   addJob,
-  deleteJob,
+  removeJob,
   updateJob,
   setError,
   resetError,
+  isInEdit,
 } = jobsSlice.actions;
 
 export default jobsSlice.reducer;
