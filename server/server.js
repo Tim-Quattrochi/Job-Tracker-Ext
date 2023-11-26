f90c44fa2c4b4e621d1f15af561139bbe2f3480e
Tim Quattrochi <timq82@gmail.com>
2023-11-25 19:37:47 -0500

    Update CORS origin in server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,12 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173/", "http://localhost:5173"],
+    origin: [
+      "http://localhost:5173/",
+      "http://localhost:5173",
+      "http://127.0.0.1:5173/login",
+      "http://127.0.0.1:5173",
+    ],
     credentials: true,
   })
 );
