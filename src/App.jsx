9c77126ca8cb6af43dde6a1021692ba6b800fea8
Tim Quattrochi <timq82@gmail.com>
2023-11-29 22:18:45 -0500

    Add Register page to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import JobTracker from "./components/JobTracker/JobTracker";
 import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 import Header from "./components/Header/Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<JobTracker />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   );
