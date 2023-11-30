e02de1fc0d66edec54164dffc1b7e15f846a8136
Tim Quattrochi <timq82@gmail.com>
2023-11-29 22:27:16 -0500

    made validation dynamic for login/reg

--- src/utilities/validateForm.js
+++ src/utilities/validateForm.js
@@ -1,22 +1,35 @@
-const validateForm = (formData) => {
+const validateForm = (formData, formType) => {
   let errors = {};
-  if (!formData.name) {
-    errors.name = "Name is required";
-  } else if (formData.name.length < 3) {
-    errors.name = "Name must be at least 3 characters";
-  } else if (!formData.email) {
-    errors.email = "Email is required";
-  } else if (!formData.password) {
-    errors.password = "Password is required";
-  } else if (formData.password.length < 6) {
-    errors.password = "Password must be at least 6 characters";
-  } else if (formData.password !== formData.confirmPassword) {
-    errors.confirmPassword = "Passwords do not match";
-  } else if (!formData.confirmPassword) {
-    errors.confirmPassword = "Confirm Password is required";
-  } else if (formData.confirmPassword.length < 6) {
-    errors.confirmPassword = "Password must be at least 6 characters";
+
+  if (formType === "register") {
+    if (!formData.email) {
+      errors.email = "Email is required";
+    }
+    if (!formData.password) {
+      errors.password = "Password is required";
+    } else if (formData.password.length < 6) {
+      errors.password = "Password must be at least 6 characters";
+    }
+    if (!formData.confirmPassword) {
+      errors.confirmPassword = "Confirm Password is required";
+    } else if (formData.confirmPassword.length < 6) {
+      errors.confirmPassword =
+        "Password must be at least 6 characters";
+    } else if (formData.password !== formData.confirmPassword) {
+      errors.confirmPassword = "Passwords do not match";
+    } else if (!formData.name) {
+      errors.name = "Name is required";
+    }
+  } else if (formType === "login") {
+    if (!formData.email) {
+      errors.email = "Email is required";
+    } else if (!formData.password) {
+      errors.password = "Password is required";
+    } else if (formData.password.length < 6) {
+      errors.password = "Password must be at least 6 characters";
+    }
   }
+
   return errors;
 };
 
